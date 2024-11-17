325c1d2af4b5db34a2f16acb1260fead5311d473
Gerald Lee <gerald13@uw.edu>
2024-11-17 09:01:20 -0800

    edits to rayleigh int and graphing functions 2

--- src/calculateRayleighIntegral.py
+++ src/calculateRayleighIntegral.py
@@ -1,3 +1,9 @@
+import settransducer
+from presets import setmedium
+trans = settransducer.setTransducer(9,0.05,0.05,1)
+medium = setmedium.setMedium('Water',dict())
+field = dict(numAxialStep = 100, numRadialStep= 100)
+
 def generateField(trans,medium,field):
     """
     INPUT ARG
@@ -35,9 +41,9 @@ def generateField(trans,medium,field):
     axial_max = 2*d
     radial_min = -trans["radius"]
     dz = (axial_max - axial_min) / field["numAxialStep"]
-    numZ = np.round((axial_max - axial_min)/dz)+1
+    numZ = int(np.round((axial_max - axial_min)/dz)+1)
     dr = -1 * radial_min / (field["numRadialStep"]/2)
-    numR = np.round((0 - radial_min)/dz)+1
+    numR = int(np.round((0 - radial_min)/dr)+1)
     # Theta Component
     thetaMax = np.arcsin(angularF)
     numT = 100
@@ -60,29 +66,43 @@ def generateField(trans,medium,field):
                 numP = (2*tt+1)
                 dphi = (2*np.pi)/numP
                 e1 = 0
-                for pp in range(0,len(numP)-1):
+                for pp in range(0,numP-1):
                     phi = dphi * pp
-                    rf = np.sqrt((pow((d*np.sin(theta)),2))+r*r-2*np.sin(theta)*np.abs(r)*d*np.cos(phi)+pow((z-d+d*np.cos(theta)),2))
+                    rf = np.sqrt((pow((d*np.sin(theta)),2))+r*r-2*np.sin(theta)*np.absolute(r)*d*np.cos(phi)+pow((z-d+d*np.cos(theta)),2))
                     # rf = np.sqrt(((d*np.sin(theta))^2)+r*r-2*np.sin(theta)*np.abs(r)*d*np.cos(phi)+(z-d+d*np.cos(theta))^2)
                     e1 = e1 + np.exp(1j*k*rf)/rf
                 p = p + e1*np.sin(theta)/(2*tt+1)
             amplitude = abs(p)*k*d*d*dtheta*np.exp(-abs_Coeff*(z-axial_min))
             pressure_field[-(zz+1)][-(rr+1)] = amplitude
             r_values[-(rr+1)] = r
-            r = r + dr
+            r = np.round(r + dr,3)
         z_values[-(zz+1)] = z
-        z = z + dz 
+        z = np.round(z + dz,3)
+        percent = np.round(100 * (zz+1)/numZ)
+        print(percent,'%')
     
+    # Set Bottom Halves
+    r_value_bothalf = r_values
+    pressure_field_bothalf = pressure_field
+
     # Radial Symmetry (reflect pressure values across z-axis)
-    for ii in range(1,numZ):
-        for jj in range(1,numR):
-            if jj != numR:
-                r_values[-(2*numR-jj)] = np.abs(r_values[-jj])
-                pressure_field[-ii][-(2*numR-jj)] = pressure_field[-ii][-jj]
+    pressure_field_tophalf = np.flipud(pressure_field_bothalf)
+    pressure_field_new = np.hstack((pressure_field_tophalf, pressure_field_bothalf[1:,:]))
+    r_value_tophalf = np.flipud(np.absolute(r_value_bothalf[1:]))
+    r_value_new = np.concatenate((r_value_tophalf,r_value_bothalf))
 
     # Fix Dividing by 0 at the first center point
-    r_CenterIdx = round((-1*radial_min/dr)+1)
-    pressure_field[-1][-r_CenterIdx] = 0
+    r_CenterIdx = round((-1*radial_min/dr))
+    print(r_CenterIdx)
+    pressure_field_new[0,r_CenterIdx] = 0
+    # for ii in range(1,numZ):
+    #     for jj in range(1,numR):
+    #         if jj != numR:
+    #             r_value_new = np.absolute(r_value_half[jj]) + r_value_new
+    #             pressure_field[ii][(2*numR-jj)] = pressure_field[ii][jj]
 
 
-    return pressure_field, z_values, r_values
+    print('YAY')
+    return pressure_field_new, z_values, r_value_new
+    # pressure_field, z_values, r_values = generateField(trans,medium,field)
+
