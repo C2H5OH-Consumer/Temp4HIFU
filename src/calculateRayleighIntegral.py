bfcf87ab969dd908d19f5e703874ac78989d1769
Gerald Lee <gerald13@uw.edu>
2024-11-24 20:50:14 -0800

    Pandas CSV Loading System

--- src/calculateRayleighIntegral.py
+++ src/calculateRayleighIntegral.py
@@ -19,13 +19,13 @@ def generateField(trans,medium,field,iscomplete=0):
 
     """
     import numpy as np
+    import pandas as pd
 
     # Edit and Transform Transducer Properties
     d = trans["focus"]
     k = 2 * np.pi * trans["freq"] / medium["speed"] # Wave Number
     angularF = trans["radius"]/trans["focus"] # Angular Frequency
     abs_Coeff = medium["absCoeff"] * (pow((trans["freq"]/(1e6)),2)) # POWER LAW
-
 
     # Set Axes
     axial_min = 0.001
@@ -42,31 +42,33 @@ def generateField(trans,medium,field,iscomplete=0):
 
 
     # Preallocation
-    z_values = np.zeros((numZ,1))
-    r_values = np.zeros((numR,1))
-    pressure_field = np.zeros((numZ,numR))
+    z_values = list()
+    r_values = list()
+    pressure_field = np.empty((numZ,numR), dtype=float)
 
     # Rayleigh Integral
     z = axial_min
-    for zz in range(0,numZ-1,1):
+    for zz in range(0,numZ):
         r = radial_min
-        for rr in range(0,numR-1,1):
+        for rr in range(0,numR):
             p = 0.5 * np.exp(1j * k * np.sqrt(z*z + r*r)) / np.sqrt(z*z + r*r) * dtheta
-            for tt in range(1,numT-1):
+            for tt in range(1,numT+1):
                 theta = tt * dtheta
                 numP = (2*tt+1)
                 dphi = (2*np.pi)/numP
                 e1 = 0
-                for pp in range(0,numP-1):
+                for pp in range(0,numP):
                     phi = dphi * pp
                     rf = np.sqrt((pow((d*np.sin(theta)),2))+r*r-2*np.sin(theta)*np.absolute(r)*d*np.cos(phi)+pow((z-d+d*np.cos(theta)),2))
                     e1 = e1 + np.exp(1j*k*rf)/rf
                 p = p + e1*np.sin(theta)/(2*tt+1)
             amplitude = abs(p)*k*d*d*dtheta*np.exp(-abs_Coeff*(z-axial_min))
-            pressure_field[(zz+1),(rr+1)] = amplitude
-            r_values[(rr)] = r
+            pressure_field[zz,rr] = amplitude
+            if zz == 0: 
+                r_values.append(r)
             r = r + dr
-        z_values[(zz)] = z
+
+        z_values.append(z)
         z = z + dz
         print((np.round(100 * (zz+1)/numZ)),'%')
     
@@ -76,7 +78,7 @@ def generateField(trans,medium,field,iscomplete=0):
 
     # Radial Symmetry (reflect pressure values across z-axis)
     pressure_field_tophalf = np.fliplr(pressure_field_bothalf)
-    pressure_field_new = np.rot90(np.hstack((pressure_field_bothalf,pressure_field_tophalf)),1)
+    pressure_field_new = np.rot90(np.hstack((pressure_field_bothalf,pressure_field_tophalf[:,1:])),1)
     r_value_tophalf = np.flipud(np.absolute(r_value_bothalf))
     r_value_new = np.concatenate((r_value_bothalf,r_value_tophalf[1:]))
 
